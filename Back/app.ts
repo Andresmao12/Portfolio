52f0ccfed797c8919529487f97e482d937e8a49d
mauro <andresmao205@gmail.com>
2026-06-23 15:08:34 -0500

    fix: server port to Rest service

--- Back/app.ts
+++ Back/app.ts
@@ -7,7 +7,7 @@ dotenv.config();
 import contactRouter from './routes/contact.routes';
 
 const app = express();
-const PORT = 5000
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -17,5 +17,5 @@ app.use('/api/contact', contactRouter);
 
 
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+    console.log(`Servidor corriendo en puerto ${PORT}`);
 });
